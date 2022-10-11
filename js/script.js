95e8629486e572447b2dc78d1be26161613cf1ae
MikeyGloriani <MikeyGloriani@users.noreply.github.com>
2022-10-11 17:41:02 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -7,9 +7,9 @@
 /**
  * Do basic math.
  */
- function doMathClicked() {
+function doMathClicked() {
   document.getElementById("area-math").innerHTML =
     "<p>The area is:" + 5 * 3 + "cm²" + "</p>"
   document.getElementById("perimeter-math").innerHTML =
-    "<p>The perimeter is:" + 2*(5 + 3) + "cm" + "</p>"
+    "<p>The perimeter is:" + 2 * (5 + 3) + "cm" + "</p>"
 }
